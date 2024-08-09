39b05129bc2cbe203e56543ae37b9beb2f2f82cb
Nandini0414 <nandinireddi01@gmail.com>
2024-08-09 15:47:54 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,238 @@
+let text=document.getElementById('text');
+let para=document.getElementsByClassName('para');
+let para1=document.getElementsByClassName('para1');
+let nandu=document.getElementsByClassName('nandu');
+
+para[0].addEventListener('click',() =>{
+    text.style.backgroundColor="aqua "
+});
+para[1].addEventListener('click',() =>{
+    text.style.backgroundColor="brown "
+});
+para[2].addEventListener('click',() =>{
+    text.style.backgroundColor="rgb(223, 237, 70 "
+});
+para[3].addEventListener('click',() =>{
+    text.style.backgroundColor="bisque "
+});
+para[4].addEventListener('click',() =>{
+    text.style.backgroundColor="blue"
+});
+para[5].addEventListener('click',() =>{
+    text.style.backgroundColor="orangered"
+});
+para[6].addEventListener('click',() =>{
+    text.style.backgroundColor="darkkhaki "
+});
+para[7].addEventListener('click',() =>{
+    text.style.backgroundColor="lightpink "
+});
+para[8].addEventListener('click',() =>{
+    text.style.backgroundColor="rgb(128, 209, 7)"
+});
+para[9].addEventListener('click',() =>{
+    text.style.backgroundColor="lightblue "
+});
+para[10].addEventListener('click',() =>{
+    text.style.backgroundColor="navajowhite "
+});
+para[11].addEventListener('click',() =>{
+    text.style.backgroundColor="purple"
+});
+para[12].addEventListener('click',() =>{
+    text.style.backgroundColor="seagreen"
+});
+para[13].addEventListener('click',() =>{
+    text.style.backgroundColor=" aqua"
+});
+para[14].addEventListener('click',() =>{
+    text.style.backgroundColor="bisque"
+});
+para[15].addEventListener('click',() =>{
+    text.style.backgroundColor="  green"
+});
+para[16].addEventListener('click',() =>{
+    text.style.backgroundColor=" fuchsia"
+});
+para[17].addEventListener('click',() =>{
+    text.style.backgroundColor="lightcyan"
+});
+para[18].addEventListener('click',() =>{
+    text.style.backgroundColor="navy "
+});
+para[19].addEventListener('click',() =>{
+    text.style.backgroundColor=" gold"
+});
+para[20].addEventListener('click',() =>{
+    text.style.backgroundColor=" lightskyblue "
+});
+para[21].addEventListener('click',() =>{
+    text.style.backgroundColor="pink"
+});
+para[22].addEventListener('click',() =>{
+    text.style.backgroundColor="olivedrab"
+});
+para[23].addEventListener('click',() =>{
+    text.style.backgroundColor="rgb(241, 10, 241) "
+});
+para[24].addEventListener('click',() =>{
+    text.style.backgroundColor="black"
+   
+});
+para[25].addEventListener('click',() =>{
+    text.style.backgroundColor="wheat "
+});
+para[26].addEventListener('click',() =>{
+    text.style.backgroundColor="peru"
+});
+para[27].addEventListener('click',() =>{
+    text.style.backgroundColor="lightgreen"
+    t
+});
+para[28].addEventListener('click',() =>{
+    text.style.backgroundColor="deeppink"
+});
+para[29].addEventListener('click',() =>{
+    text.style.backgroundColor="khaki"
+});
+para1[0].addEventListener('click',() =>{
+    text.style.color="aqua "
+});
+para1[1].addEventListener('click',() =>{
+    text.style.color="brown "
+});
+para1[2].addEventListener('click',() =>{
+    text.style.color="rgb(223, 237, 70 "
+});
+para1[3].addEventListener('click',() =>{
+    text.style.color="bisque "
+});
+para1[4].addEventListener('click',() =>{
+    text.style.color="blue"
+});
+para1[5].addEventListener('click',() =>{
+    text.style.color="orangered"
+});
+para1[6].addEventListener('click',() =>{
+    text.style.color="darkkhaki "
+});
+para1[7].addEventListener('click',() =>{
+    text.style.color="lightpink "
+});
+para1[8].addEventListener('click',() =>{
+    text.style.color="rgb(128, 209, 7)"
+});
+para1[9].addEventListener('click',() =>{
+    text.style.color="lightblue "
+});
+para1[10].addEventListener('click',() =>{
+    text.style.color="navajowhite "
+});
+para1[11].addEventListener('click',() =>{
+    text.style.color="purple"
+});
+para1[12].addEventListener('click',() =>{
+    text.style.color="aqua"
+});
+para1[13].addEventListener('click',() =>{
+    text.style.color=" seagreen"
+});
+para1[14].addEventListener('click',() =>{
+    text.style.color="aqua"
+});
+para1[15].addEventListener('click',() =>{
+    text.style.color="  bisque"
+});
+para1[16].addEventListener('click',() =>{
+    text.style.color="  green"
+});
+
+para1[17].addEventListener('click',() =>{
+    text.style.color=" fuchsia"
+});
+para1[18].addEventListener('click',() =>{
+    text.style.color="lightcyan"
+});
+para1[19].addEventListener('click',() =>{
+    text.style.color="navy "
+});
+para1[20].addEventListener('click',() =>{
+    text.style.color=" gold"
+});
+para1[21].addEventListener('click',() =>{
+    text.style.color=" lightskyblue "
+});
+para1[22].addEventListener('click',() =>{
+    text.style.color="pink"
+});
+para1[23].addEventListener('click',() =>{
+    text.style.color="olivedrab"
+});
+para1[24].addEventListener('click',() =>{
+    text.style.color="rgb(241, 10, 241) "
+});
+para1[25].addEventListener('click',() =>{
+    text.style.color="black"
+   
+});
+para1[26].addEventListener('click',() =>{
+    text.style.color="wheat "
+});
+para1[27].addEventListener('click',() =>{
+    text.style.color="lightgreen"
+    
+});
+para1[28].addEventListener('click',() =>{
+    text.style.color="peru"
+});
+
+para1[29].addEventListener('click',() =>{
+    text.style.color="deeppink"
+});
+
+nandu[0].addEventListener('click',() =>{
+    text.style.fontFamily="'Courier New', Courier, monospace"
+})
+nandu[1].addEventListener('click',() =>{
+    text.style.fontFamily="'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
+})
+nandu[2].addEventListener('click',() =>{
+    text.style.fontFamily=" 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"
+})
+nandu[3].addEventListener('click',() =>{
+    text.style.fontFamily="'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
+})
+nandu[4].addEventListener('click',() =>{
+    text.style.fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
+})
+nandu[5].addEventListener('click',() =>{
+    text.style.fontFamily="'Times New Roman', Times, serif"
+})
+nandu[6].addEventListener('click',() =>{
+    text.style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
+})
+
+nandu[7].addEventListener('click',() =>{
+    text.style.fontFamily="Arial, Haelvetic, sans-serif"
+})
+nandu[8].addEventListener('click',() =>{
+    text.style.fontFamily="Georgia, 'Times New Roman', Times, serif"
+})
+nandu[9].addEventListener('click',() =>{
+    text.style.fontFamily="Verdana, Geneva, Tahoma, sans-serif"
+})
+
+let d = document.getElementById('download');
+d.addEventListener('click', download_image);
+function download_image(){
+    html2canvas(document.getElementById('text')).then(canvas=>{
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'image.png';
+        link.click();
+    });
+}
+
+
+
+
